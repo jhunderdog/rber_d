324f28102914e8698101fd15207fb79fbb1debca
jhunderdog <33382water@gmail.com>
2021-01-17 21:38:24 +0900

    first commit

---
+++ lib/AllScreens/mainscreen.dart
@@ -0,0 +1,82 @@
+import 'package:drivers_app/tabPages/earningsTabPage.dart';
+import 'package:drivers_app/tabPages/homeTabPage.dart';
+import 'package:drivers_app/tabPages/profileTabPage.dart';
+import 'package:drivers_app/tabPages/ratingTabPage.dart';
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  static const String idScreen = "mainScreen";
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen>
+    with SingleTickerProviderStateMixin {
+  TabController tabController;
+  int selectedIndex = 0;
+
+  void onItemClicked(int index) {
+    setState(() {
+      selectedIndex = index;
+      tabController.index = selectedIndex;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    tabController =
+        TabController(length: 4, vsync: this); //SingleTikcer What's this?
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    tabController.dispose(); //어떻게 작동?
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: TabBarView(
+        physics: NeverScrollableScrollPhysics(),
+        controller: tabController,
+        children: [
+          HomeTabPage(),
+          EarningsTabPage(),
+          RatingTabPage(),
+          ProfileTabPage(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.credit_card),
+            label: "Earnings",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: "Ratings",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Account",
+          ),
+        ],
+        unselectedItemColor: Colors.black54,
+        selectedItemColor: Colors.yellow,
+        type: BottomNavigationBarType.fixed,
+        selectedLabelStyle: TextStyle(fontSize: 12.0),
+        showUnselectedLabels: true,
+        currentIndex: selectedIndex,
+        onTap: onItemClicked,
+      ),
+    );
+  }
+}
---
+++ lib/AllScreens/registerationScreen.dart
@@ -0,0 +1,231 @@
+import 'package:drivers_app/AllScreens/carInfoScreen.dart';
+import 'package:drivers_app/configMaps.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:drivers_app/AllScreens/loginScreen.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:drivers_app/AllScreens/mainscreen.dart';
+import 'package:drivers_app/AllWidgets/progressDialog.dart';
+import 'package:drivers_app/main.dart';
+
+class RegisterationScreen extends StatelessWidget {
+  static const String idScreen = "register";
+  TextEditingController nameTextEditingController = TextEditingController();
+  TextEditingController emailTextEditingController = TextEditingController();
+  TextEditingController phoneTextEditingController = TextEditingController();
+  TextEditingController passwordTextEditingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 1.0,
+              ),
+              Image(
+                image: AssetImage("images/logo.png"),
+                width: 390.0,
+                height: 250.0,
+                alignment: Alignment.center,
+              ),
+              SizedBox(
+                height: 1.0,
+              ),
+              Text(
+                "Register as Driver",
+                style: TextStyle(fontSize: 24.0, fontFamily: "Brand Bold"),
+                textAlign: TextAlign.center,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 1.0,
+                    ),
+                    TextField(
+                      controller: nameTextEditingController,
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                        labelText: "Name",
+                        labelStyle: TextStyle(
+                          fontSize: 14.0,
+                        ),
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 10.0,
+                        ),
+                      ),
+                      style: TextStyle(
+                        fontSize: 14.0,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 1.0,
+                    ),
+                    TextField(
+                      controller: emailTextEditingController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: "Email",
+                        labelStyle: TextStyle(
+                          fontSize: 14.0,
+                        ),
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 10.0,
+                        ),
+                      ),
+                      style: TextStyle(
+                        fontSize: 14.0,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 1.0,
+                    ),
+                    TextField(
+                      controller: phoneTextEditingController,
+                      keyboardType: TextInputType.phone,
+                      decoration: InputDecoration(
+                        labelText: "Phone",
+                        labelStyle: TextStyle(
+                          fontSize: 14.0,
+                        ),
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 10.0,
+                        ),
+                      ),
+                      style: TextStyle(
+                        fontSize: 14.0,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 1.0,
+                    ),
+                    TextField(
+                      controller: passwordTextEditingController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        labelStyle: TextStyle(
+                          fontSize: 14.0,
+                        ),
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 10.0,
+                        ),
+                      ),
+                      style: TextStyle(
+                        fontSize: 14.0,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    RaisedButton(
+                      color: Colors.yellow,
+                      textColor: Colors.white,
+                      child: Container(
+                        height: 50.0,
+                        child: Center(
+                          child: Text(
+                            "Create Account",
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              fontFamily: "Brand Bold",
+                            ),
+                          ),
+                        ),
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24.0),
+                      ),
+                      onPressed: () {
+                        if (nameTextEditingController.text.length < 3) {
+                          displayToastMessage(
+                              "name must be at least 3 characters", context);
+                        } else if (!emailTextEditingController.text
+                            .contains("@")) {
+                          displayToastMessage(
+                              "Email address is not Valid", context);
+                        } else if (phoneTextEditingController.text.isEmpty) {
+                          displayToastMessage(
+                              "Phone Number is mandatory", context);
+                        } else if (passwordTextEditingController.text.length <
+                            6) {
+                          displayToastMessage(
+                              "Password must be at least 6 characters",
+                              context);
+                        } else {
+                          registerNewUser(context);
+                        }
+                      },
+                    )
+                  ],
+                ),
+              ),
+              FlatButton(
+                child: Text(
+                  "Already have an Account? Login Here.",
+                ),
+                onPressed: () {
+                  Navigator.pushNamedAndRemoveUntil(
+                      context, LoginScreen.idScreen, (route) => false);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  void registerNewUser(BuildContext context) async {
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return ProgressDialog(
+            message: "Registering, Please wait...",
+          );
+        });
+    final User firebaseUser = (await _firebaseAuth
+            .createUserWithEmailAndPassword(
+                email: emailTextEditingController.text,
+                password: passwordTextEditingController.text)
+            .catchError((errMsg) {
+      Navigator.pop(context);
+      displayToastMessage("Error:" + errMsg.toString(), context);
+    }))
+        .user;
+
+    if (firebaseUser != null) {
+      Map userDataMap = {
+        "name": nameTextEditingController.text.trim(),
+        "email": emailTextEditingController.text.trim(),
+        "phone": phoneTextEditingController.text.trim(),
+      };
+
+      driversRef.child(firebaseUser.uid).set(userDataMap);
+      currentfirebaseUser = firebaseUser;
+      displayToastMessage(
+          "Congratulations, your account has been created.", context);
+      Navigator.pushNamed(context, CarInfoScreen.idScreen);
+    } else {
+      Navigator.pop(context);
+      displayToastMessage("New user has not been Created.", context);
+    }
+  }
+}
+
+displayToastMessage(String message, BuildContext context) {
+  Fluttertoast.showToast(msg: message);
+}
