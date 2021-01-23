35a5ef88fd2c869dfc2084f3bec4973f77f7eb83
jhunderdog <33382water@gmail.com>
2021-01-23 11:13:30 +0900

    request accepted

--- lib/AllScreens/newRideScreen.dart
+++ lib/AllScreens/newRideScreen.dart
@@ -1,22 +1,160 @@
+import 'dart:async';
+
 import 'package:drivers_app/Models/rideDetails.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class NewRideScreen extends StatefulWidget {
   final RideDetails rideDetails;
   NewRideScreen({this.rideDetails});
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37, -122),
+    zoom: 14.4746,
+  );
   @override
   _NewRideScreenState createState() => _NewRideScreenState();
 }
 
 class _NewRideScreenState extends State<NewRideScreen> {
+  Completer<GoogleMapController> _controllerGoogleMap = Completer();
+  GoogleMapController newRideGoogleMapController;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("New Ride"),
-      ),
-      body: Center(
-        child: Text("This is New Ride Page"),
+      body: Stack(
+        children: [
+          GoogleMap(
+            mapType: MapType.normal,
+            myLocationButtonEnabled: true,
+            initialCameraPosition: NewRideScreen._kGooglePlex,
+            myLocationEnabled: true,
+            onMapCreated: (GoogleMapController controller) {
+              _controllerGoogleMap.complete(controller);
+              newRideGoogleMapController = controller;
+            },
+          ),
+          Positioned(
+            left: 0.0,
+            right: 0.0,
+            bottom: 0.0,
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(16.0),
+                      topRight: Radius.circular(16.0)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black38,
+                      blurRadius: 16.0,
+                      spreadRadius: 0.5,
+                      offset: Offset(0.7, 0.7),
+                    )
+                  ]),
+              height: 260.0,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 18.0),
+                child: Column(
+                  children: [
+                    Text(
+                      "10 mins",
+                      style: TextStyle(
+                          fontSize: 14.0,
+                          fontFamily: "Brand Bold",
+                          color: Colors.deepPurple),
+                    ),
+                    SizedBox(
+                      height: 6.0,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Muhammad Ali",
+                          style: TextStyle(
+                              fontFamily: "Brand Bold", fontSize: 24.0),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(right: 10.0),
+                          child: Icon(Icons.phone_android),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 26.0,
+                    ),
+                    Row(
+                      children: [
+                        Image.asset(
+                          "images/pickicon.png",
+                          height: 16.0,
+                          width: 16.0,
+                        ),
+                        SizedBox(
+                          width: 18.0,
+                        ),
+                        Expanded(
+                          child: Container(
+                            child: Text(
+                              "Street#44, Paris, France.",
+                              style: TextStyle(fontSize: 18.0),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 26.0,
+                    ),
+                    Row(
+                      children: [
+                        Image.asset(
+                          "images/desticon.png",
+                          height: 16.0,
+                          width: 16.0,
+                        ),
+                        SizedBox(
+                          width: 18.0,
+                        ),
+                        Expanded(
+                          child: Container(
+                            child: Text(
+                              "Street#88, Paris, France.",
+                              style: TextStyle(fontSize: 18.0),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16.0),
+                      child: RaisedButton(
+                        onPressed: () {},
+                        color: Theme.of(context).accentColor,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("Arrived",
+                                style: TextStyle(
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white)),
+                            Icon(Icons.directions_car,
+                                color: Colors.white, size: 26.0),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
--- lib/Assistants/assistantMethods.dart
+++ lib/Assistants/assistantMethods.dart
@@ -11,31 +11,31 @@ import 'package:drivers_app/Models/directDetails.dart';
 import 'package:drivers_app/configMaps.dart';
 
 class AssistantMethods {
-  static Future<String> searchCoordinateAddress(
-      Position position, context) async {
-    String placeAddress = "";
-    String st1, st2, st3, st4;
-    String url =
-        "https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.latitude},${position.longitude}&key=$mapKey";
-    var response = await RequestAssistant.getRequest(url);
-    if (response != "Failed.") {
-      placeAddress = response["results"][0]["formatted_address"];
-      // st1 = response["results"][0]["address_components"][3]["long_name"];
-      // st2 = response["results"][0]["address_components"][4]["long_name"];
-      // st3 = response["results"][0]["address_components"][5]["long_name"];
-      // st4 = response["results"][0]["address_components"][6]["long_name"];
-      // placeAddress = st1 + ", " + st2 + ", " + st3 + ", " + st4;
-
-      Address userPickUpAddress = Address();
-      userPickUpAddress.longitude = position.longitude;
-      userPickUpAddress.latitude = position.latitude;
-      userPickUpAddress.placeName = placeAddress;
-
-      Provider.of<AppData>(context, listen: false)
-          .updatePickUpLocationAddress(userPickUpAddress);
-    }
-    return placeAddress;
-  }
+  // static Future<String> searchCoordinateAddress(
+  //     Position position, context) async {
+  //   String placeAddress = "";
+  //   String st1, st2, st3, st4;
+  //   String url =
+  //       "https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.latitude},${position.longitude}&key=$mapKey";
+  //   var response = await RequestAssistant.getRequest(url);
+  //   if (response != "Failed.") {
+  //     placeAddress = response["results"][0]["formatted_address"];
+  //     // st1 = response["results"][0]["address_components"][3]["long_name"];
+  //     // st2 = response["results"][0]["address_components"][4]["long_name"];
+  //     // st3 = response["results"][0]["address_components"][5]["long_name"];
+  //     // st4 = response["results"][0]["address_components"][6]["long_name"];
+  //     // placeAddress = st1 + ", " + st2 + ", " + st3 + ", " + st4;
+  //
+  //     Address userPickUpAddress = Address();
+  //     userPickUpAddress.longitude = position.longitude;
+  //     userPickUpAddress.latitude = position.latitude;
+  //     userPickUpAddress.placeName = placeAddress;
+  //
+  //     Provider.of<AppData>(context, listen: false)
+  //         .updatePickUpLocationAddress(userPickUpAddress);
+  //   }
+  //   return placeAddress;
+  // }
 
   static Future<DirectionDetails> obtainDirectionDetails(
       LatLng initialPosition, LatLng finalPosition) async {
@@ -69,15 +69,15 @@ class AssistantMethods {
     return totalFareAmount.truncate();
   }
 
-  static void getCurrentOnlineUserInfo() async {
-    firebaseUser = await FirebaseAuth.instance.currentUser;
-    String userId = firebaseUser.uid;
-    DatabaseReference reference =
-        FirebaseDatabase.instance.reference().child("users").child(userId);
-    reference.once().then((DataSnapshot dataSnapShot) {
-      if (dataSnapShot.value != null) {
-        userCurrentInfo = Users.fromSnapshot(dataSnapShot);
-      }
-    });
-  }
+  // static void getCurrentOnlineUserInfo() async {
+  //   firebaseUser = await FirebaseAuth.instance.currentUser;
+  //   String userId = firebaseUser.uid;
+  //   DatabaseReference reference =
+  //       FirebaseDatabase.instance.reference().child("users").child(userId);
+  //   reference.once().then((DataSnapshot dataSnapShot) {
+  //     if (dataSnapShot.value != null) {
+  //       userCurrentInfo = Users.fromSnapshot(dataSnapShot);
+  //     }
+  //   });
+  // }
 }
